57890c7555d9130cf9426dcb2af8cf74996a0492
bem13 <bem13@freemail.hu>
2014-04-25 01:59:11 +0200

    imgur_scraper.py

---
+++ imgur_scraper.py
@@ -0,0 +1,54 @@
+import urllib2
+import random
+import os
+import math
+
+path = os.path.abspath('./imgur_scraper')
+string_length = 5 # The length of the ID to generate. 5 seems to work the best.
+
+def RandomID(string_length):
+	chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	output = ''
+	index = 0
+	for i in range(0, string_length):
+		index = math.floor(random.random() * len(chars))
+		output += chars[int(index):int(index + 1)]
+	return output
+
+print 'Location of saved files: %s' % path
+if not os.path.exists(path):
+	os.makedirs(path)
+	
+while True:
+	id = RandomID(string_length)
+	filename = ''
+	url = 'http://i.imgur.com/' + id + '.jpg'
+	try:
+		res = urllib2.urlopen(url)
+	except urllib2.HTTPError as e:
+		print '%s - error %d' % (id, e.code)
+		continue
+	data = res.read()
+	type = res.info()['Content-Type']
+	if res.geturl() == 'http://i.imgur.com/removed.png' or type == 'text/html; charset=utf-8' or type == 'binary/octet-stream':
+		print '%s - not found' % id
+	else:
+		if type == 'image/jpeg':
+			id += '.jpg'
+		elif type == 'image/png':
+			id += '.png'
+		elif type == 'image/gif':
+			id += '.gif'
+		elif type == 'image/bmp':
+			id += '.bmp'
+		else:
+			print 'Type could not be recognized!'
+			continue
+		
+		filename = os.path.join(path, id)
+		if os.path.exists(filename):
+			print 'Already exists: %s' % id
+		f = open(filename, 'wb')
+		f.write(data)
+		f.close()
+		print 'Downloaded: %s' % id
